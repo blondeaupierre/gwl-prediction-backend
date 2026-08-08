4ab584319084ed042b5e6c075fb7afef4476bb85
blondeaupierre <niono235@gmail.com>
2026-08-08 20:03:15 +0200

    Update origin for dns

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://167.233.65.209",
+    allow_origins=["https://meteonappes.fr",
                    "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
